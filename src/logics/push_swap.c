022f76d652310c613ab8a5d5f6aa35ed0187a42b
azinchen <azinchen@student.hive.fi>
2024-08-19 00:13:24 +0300

    1 minor fix in main about exit

--- src/logics/push_swap.c
+++ src/logics/push_swap.c
@@ -21,7 +21,9 @@ int main(int argc, char *argv[])
 
 	a = NULL;
 	b = NULL;
-	if (argc == 1 || (argc == 2 && !argv[1][0])) //second argument is what exactly? \0?
+	if (argc == 1) 
+		return (0);
+	if (argc == 2 && !argv[1][0]) //second argument is what exactly? \0?
 		error_and_exit(); // can be -1? //WHAT SHOULD BE HERE IF 0 ARGs???
 	numbers = argv + 1;
 	if (argc == 2)
